bcd8687e79fced7e23054d30955275cf8535c931
whoisnian <zhuchangbao1998@gmail.com>
2023-02-02 12:38:25 +0800

    FIX: rails cookie message metadata

--- crypto/json_msg_serializer.go
+++ crypto/json_msg_serializer.go
@@ -1,10 +1,19 @@
 package crypto
 
 import (
+	"encoding/base64"
 	"encoding/json"
 )
 
 type JsonMsgSerializer struct {
+}
+
+type JsonMsgMetadata struct {
+	Rails struct {
+		Message string          `json:"message"`
+		Exp     json.RawMessage `json:"exp"`
+		Pur     json.RawMessage `json:"pur"`
+	} `json:"_rails"`
 }
 
 func (s JsonMsgSerializer) Serialize(v interface{}) (string, error) {
@@ -16,5 +25,14 @@ func (s JsonMsgSerializer) Serialize(v interface{}) (string, error) {
 }
 
 func (s JsonMsgSerializer) Unserialize(data string, v interface{}) error {
-	return json.Unmarshal([]byte(data), v)
+	bs := []byte(data)
+	meta := JsonMsgMetadata{}
+	err := json.Unmarshal(bs, &meta)
+	if err == nil && len(meta.Rails.Message) > 0 {
+		if bs, err = base64.StdEncoding.DecodeString(meta.Rails.Message); err != nil {
+			return err
+		}
+	}
+
+	return json.Unmarshal(bs, v)
 }
